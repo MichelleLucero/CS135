739281ba96341678fb8b32c897738069dab7522b
mlucero4607 <michelle.lucero07@myhunter.cuny.edu>
2018-03-30 12:25:39 -0400

    fixing identical

--- Proj_02/pronounce.cpp
+++ Proj_02/pronounce.cpp
@@ -38,6 +38,9 @@ string transformW(string W){
 }
 
 
+
+
+
 string findWord(string W){
 
     ifstream inFile;
@@ -64,12 +67,13 @@ string findWord(string W){
             found = true; 
             break;
             return afterSpace;
+
+            
         } 
 
              
 
     }
-
     if(!found){
         cout<<"Not Found"<<endl;
         }
@@ -77,6 +81,47 @@ string findWord(string W){
     inFile.close();
 }
 
+
+
+
+
+string identical(string W){
+
+    ifstream inFile;
+    inFile.open("cmudict.0.7a.txt"); //take out txt when submitting
+
+    string beforeSpace;
+    string afterSpace;
+    string result;
+    string line;
+    string pronun;
+
+    //string pronunciation;
+
+    //Check For error
+    if(inFile.fail()){
+        cerr << "Error Opening File" << endl;
+        exit(1);
+    }
+        //bool found = false; //condition set to cout in case of Not Found
+        while(!inFile.eof()){
+        getline(inFile, line);
+        splitOnSpace(line,beforeSpace,afterSpace);
+        
+        if(W == beforeSpace){
+            pronun = afterSpace; //currPronun
+        inFile.clear();//Clear file handler.
+        inFile.seekg(0, ios_base::beg);//Go back to beginning.
+        }
+            if(pronun == afterSpace && beforeSpace != W){ //does not account for words with the same pronun before the inputed word
+                cout<< beforeSpace << endl;
+                result += beforeSpace + " ";
+            }              
+
+    }
+    inFile.close();
+    return result;
+}
 
 
 
@@ -88,6 +133,8 @@ int main(){
 
     string upperW = transformW(W);
     
-    cout<< findWord(upperW)<<endl;
+    //cout<< findWord(upperW)<<endl;
+
+    cout<< identical(upperW) <<endl;
 
 }
