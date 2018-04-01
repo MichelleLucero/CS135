50c70f6465c5a6b6ea5cdd62ae6e0c6a49a5b856
mlucero4607 <michelle.lucero07@myhunter.cuny.edu>
2018-03-31 22:14:33 -0400

    working on addPhoneme

--- Proj_02/pronounce.cpp
+++ Proj_02/pronounce.cpp
@@ -128,30 +128,79 @@ int countPhoneme(string W){
     return count;
 }
 
+string fixPro(string pro){ // fixed pronunciation's first space problem
+    int length = pro.length();
+    string fixed;
+    for(int i = 1; i <length; i++){
+        fixed += pro[i];
+    }
+    return fixed;
+}
 
 string nthPhoneme(string W, int pos){ 
+    
+    string fix = fixPro(W); // "P L AE1 N T S" instead of " P L AE1 N T S"
 
     int numPho = countPhoneme(W); // number of phonemes in the pronuncaition
 
     string beforeSpace;
     string afterSpace;
-    string desiredPho;//the nth Phoneme 
+    string desiredPho; //the nth Phoneme 
     
     for(int i = 0; i < numPho; i++){
-        splitOnSpace(W, beforeSpace, afterSpace);//W pronuncaition
-        cout<<W<<endl; //testing and it does print out the pronuncaition
-        cout<<beforeSpace<<endl; // it doesn't print out beforeSpace idk why???
-        
-        if(i == pos){
-            desiredPho = beforeSpace; //should grab the desired phoneme
 
+        splitOnSpace(fix, beforeSpace, afterSpace);//W pronuncaition
+
+        if(i == pos){
+
+            desiredPho = beforeSpace; //grabs the desired phoneme
         }
-        else{
-            W = beforeSpace; // resets W to beforeSpace for next iteration
+        else if(i != pos){
+            fix = afterSpace; // resets fix to beforeSpace for next iteration
         }
     }
-
     return desiredPho; 
+}
+
+string addPhoneme(string W){ 
+    ifstream inFile;
+    inFile.open("cmudict.0.7a.txt"); //take out .txt when submitting
+
+    string beforeSpace;
+    string afterSpace;
+    string result;
+    string line;
+    string pronun = pronunciation(W);
+    string fix = fixPro(pronun); //fixes the first space problem
+    int numofPho = countPhoneme(W);
+    int counter = 0; //keeps count of how many phonemes are similar b/w two pronunciations 
+    //Check For error
+    if(inFile.fail()){
+        cerr << "Error Opening File" << endl;
+        exit(1);
+    }
+
+        while(!inFile.eof()){
+        getline(inFile, line);
+        splitOnSpace(line,beforeSpace,afterSpace); //need to count afterSpace phoneme
+        
+        if(numofPho == countPhoneme(afterSpace)-1){ // checks if pronunciation has one more than the OG pronunciation
+            //result += beforeSpace + " ";
+            string fixAfter = fixPro(afterSpace); //fixes afterSpace first space problem
+            for(int i = 0; i< numofPho; i++){ //gotta use nthPhoneme(string W, int pos)
+                if(nthPhoneme(fix,i) == nthPhoneme(fixAfter,i) || nthPhoneme(fix,i) == nthPhoneme(fixAfter,i+1)){
+                    counter += 1;
+                }
+            }
+            if(counter == numofPho){
+                result += beforeSpace + " ";
+            }
+        }
+               
+    }
+
+    inFile.close();
+    return result;
 }
 
 
@@ -174,8 +223,11 @@ int main(){
     cout<<"Identical:         "<< identical(upperW) <<endl;
 
     //cout<<"number of pho     "<<countPhoneme(P)<<endl;
+    //cout<<fixPro(P);
 
-    cout<< nthPhoneme(P,1)<<endl;
+    //cout<< nthPhoneme(P,1)<<endl;
+
+    cout<<addPhoneme(P)<<endl;
 
 
 }
